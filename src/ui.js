d1fbf316e9c631cebe93c4cc27c50b384fd13bd5
Burgess Leon Macintosh Doan III <40319565+Burgintosh@users.noreply.github.com>
2020-01-20 13:16:25 -0500

    multicamera support (Not fully implemented)

--- src/ui.js
+++ src/ui.js
@@ -213,8 +213,17 @@ NetworkTables.addKeyListener('/SmartDashboard/Cargo:HasHatch', (key, value) => {
 // ========================================================================================
 // Spinner
 // ========================================================================================
-NetworkTables.addKeyListener('/SmartDashboard/')
+NetworkTables.addKeyListener('/SmartDashboard/Spinner:Rotations'), (key, value) => {
+	if (value >= 3 && value <=5) {
+		ui.spinnerRotations.style = "background-color:green;";
+	} else {
+		ui.spinnerRotations.style = "background-color:red;";
+	}
+}
 
+NetworkTables.addKeyListener('/SmartDashboard/Spinner:Color'), (key, value) => {
+	
+}
 // ========================================================================================
 // misc 
 // ========================================================================================
